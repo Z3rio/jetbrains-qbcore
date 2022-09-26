72ff0cf4fe68636bf0e0e28d713041f874223aaa
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-09-26 17:32:20 +0200

    Start work on building

--- src/main.js
+++ src/main.js
@@ -1,6 +1,55 @@
-var fs = require("fs");
+const fs = require("fs");
+const fsExtra = require("fs-extra");
 
-fs.appendFile("./build/test.lua", 'print("yes")', function (err) {
-  if (err) throw err;
-  console.log("Saved!");
-});
+const request = require("request");
+
+// Setup build directory
+if (!fs.existsSync("./build")) {
+  fs.mkdirSync("./build");
+} else {
+  fsExtra.emptyDirSync("./build");
+}
+
+if (!fs.existsSync("./build/zerio-intellij")) {
+  fs.mkdirSync("./build/zerio-intellij");
+}
+
+if (!fs.existsSync("./build/zerio-intellij/QBCore")) {
+  fs.mkdirSync("./build/zerio-intellij/QBCore");
+}
+
+// Get JSON data
+let data = [];
+request.get(
+  "https://raw.githubusercontent.com/Z3rio/jetbrains-qbcore/main/data/qbcore.json",
+  (error, request, content) => {
+    data = JSON.parse(content);
+
+    // Create auto-completion data files
+    for (i = 0; i < data.length; i++) {
+      let currentSection = data[i];
+      let string = "";
+
+      for (const currentFunction in currentSection.list) {
+        console.log(currentFunction);
+        console.log(currentFunction.description);
+        string += `
+            ${
+              currentFunction.description !== undefined
+                ? "--@description " + currentFunction.description + "\n"
+                : ""
+            }
+        `;
+      }
+
+      fs.appendFile(
+        `./build/zerio-intellij/QBCore/${currentSection.name}.lua`,
+        string,
+        function (err) {
+          if (err) throw err;
+          console.log(`Saved ${currentSection.name}.lua`);
+        }
+      );
+    }
+  }
+);
